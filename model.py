2b75f14a666b46dff65819bafca4a7c51ee8489b
AgustinRoca <aroca@itba.edu.ar>
2024-07-10 21:06:26 -0300

    redondeo a 4 decimales

--- model.py
+++ model.py
@@ -2,40 +2,40 @@ import pandas as pd
 
 
 def predict_valuacion_danios(row):
-    # pedido = row['ValorReclamo']
     valuacion = row['valuacion_fiscal']
     dnbr = row['dnbr']
     
-    suma = 6
+    suma = 0
     if dnbr > 0:
-        suma = 9
-    elif dnbr > 0.1:
-        suma = 10
+        suma += 1
+    if dnbr > 0.1:
+        suma += 1
 
-    # if reclamo_bajo:
-    #     predicted = valuacion * 20
-    # elif not tiene_cobertura_seguro:
-    #     predicted = 0
-    # else:
-    predicted = valuacion * 0.9 * suma / 10
+    if suma == 0:
+        coef = 6
+    elif suma == 1:
+        coef = 9
+    else:
+        coef = 10
+
+    predicted = valuacion * 0.9 * coef / 10
+    predicted = round(predicted, 4)
 
     if row['Valuacion_Danios'] > 0:
         return row['Valuacion_Danios']
     return predicted
-    
+
 if __name__ == '__main__':
     df = pd.read_csv('data/clean_processed_data.csv')
-
-    df['Valuacion_Danios_Predicted'] = df.apply(predict_valuacion_danios, axis=1)
-
-    mae = (df['Valuacion_Danios'] - df['Valuacion_Danios_Predicted']).abs().mean()
-    print(f'MAE: {mae}')
-    
-    df = df[['id', 'Valuacion_Danios_Predicted']]
     test_df = pd.read_csv('data/test.csv')
-    test_df['idParcelaWithoutPedania'] = test_df['idParcela'].str.split('-').str[0]
-    test_df['idParcelaWithoutPedania'] = test_df['idParcelaWithoutPedania'].astype('Int64')
-    merged_df = pd.merge(test_df, df, right_on='id', left_on='idParcelaWithoutPedania', how='left')
+    df.drop(columns=['idParcela'], inplace=True)
+
+    test_df['id'] = test_df['idParcela'].str.split('-').str[0]
+    test_df['id'] = test_df['id'].astype('Int64')
+
+    merged_df = pd.merge(test_df, df, right_on='id', left_on='id', how='left')
+    merged_df['Valuacion_Danios_Predicted'] = merged_df.apply(predict_valuacion_danios, axis=1)
     merged_df = merged_df[['idParcela', 'Valuacion_Danios_Predicted']]
-    # merged_df['id'] = merged_df['id'].astype('Int64')
+    # fill NaN values with 0
+    merged_df['Valuacion_Danios_Predicted'].fillna(0, inplace=True)
     merged_df.to_csv('data/submission.csv', index=False)
